f295c5be04cbd436d670d4243669d70b333b139d
Moya J <mjohnsen@gmx.com>
2016-10-18 20:21:54 -0500

    initial launch

--- scripts/updatesection.jsx
+++ scripts/updatesection.jsx
@@ -4,7 +4,8 @@ var UpdateScript = React.createClass({
 			<h1 id="update_title">Updates</h1>
 			<p className="update_header">October 19, 2016</p>
 			<p>Welcome to my site! It's still under construction at the moment, so content is a little light. Expect to see updates made to this site at least once every week from this point onwards. Definitely expect to see a few open-source projects.</p>
-			<p>If you would like to contact me, you can at moyjohnsen@aol.com</p>
+			<p>I am also opening myself to perform free web development for any non-profit causes. If you know of a non-profit cause that requires a web developer, please do not hesitate to let me know.</p>
+			<p>You can reach me at moyjohnsen@aol.com</p>
 			</div>);
 	}
 });
